82c4ee98c7c9be8ec09c4ea79c339017110c1076
Ingeborg_De_Smedt <ingeborg.de.smedt@skynent.be>
2023-11-02 10:54:50 +0100

    changed h1 into h2 since h1 already used on page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-       <h1>Test</h1> 
+       <h2>Test</h2> 
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
